fd1f5ebc1317195a4d5c5a30cdd467b56778a1cb
Nguyễn Tạ Huy Hoàng <85854007+NguyenTaHuyHoang@users.noreply.github.com>
2024-10-17 15:34:02 +0700

    validator, hashpwd, query, debug

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+  // whitelist: tránh trường hợp update thừa thông tin cho người dùng
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // Loại bỏ các trường không có
+      forbidNonWhitelisted: true,
+    }),
+  );
 
+  // Mỗi endpoint sẽ thêm tiền tố trong mỗi url
+  // nextJS global route prefix
+  app.setGlobalPrefix('api', { exclude: [''] });
   await app.listen(port);
 }
 bootstrap();
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { hashPassword } from '@/helpers/util';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  isEmailExist = async (email: string) => {
+    const user = await this.userModel.exists({ email });
+    if (user) {
+      return true;
+    } else return false;
+  };
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+    // check email
+    if (await this.isEmailExist(email)) {
+      throw new BadRequestException(
+        `Email exist: ${email}. Please, using another email`,
+      );
+    }
+
+    // hash pass
+    const hashPass = await hashPassword(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPass,
+      phone,
+      address,
+      image,
+    });
+    return { _id: user.id };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(query: string, current: number, pageSize: number) {
+    const { filter, sort } = aqp(query);
+
+    if (!current) current = 1;
+    if (!pageSize) pageSize = 10;
+
+    if (filter.current) delete filter.current;
+    if (filter.pageSize) delete filter.pageSize;
+
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    const skip = (current - 1) * pageSize;
+    const results = await this.userModel
+      .find(filter)
+      .limit(pageSize)
+      .skip(skip)
+      .select('-password')
+      .sort(sort as any);
+    return { results, totalPages };
   }
 
   findOne(id: number) {
